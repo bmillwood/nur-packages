c7a142ad6511ca40b679e22d6b50b167bdaac1f7
Ben Millwood <thebenmachine+git@gmail.com>
2026-05-24 18:19:52 +0100

    drop example package

--- default.nix
+++ default.nix
@@ -19,7 +19,7 @@
   # flakeModules = { }; # flake-parts modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  example-package = pkgs.callPackage ./pkgs/example-package { };
+  # example-package = pkgs.callPackage ./pkgs/example-package { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
