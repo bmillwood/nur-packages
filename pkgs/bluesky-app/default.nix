af54c39e3d780f92fb7c85ea51193ad4ee8a1292
Ben Millwood <thebenmachine+git@gmail.com>
2026-06-02 16:47:53 +0100

    more explanation

--- pkgs/bluesky-app/default.nix
+++ pkgs/bluesky-app/default.nix
@@ -38,6 +38,13 @@ let
       hash = "sha256-w9MF2aHw9mIoTORbgNtvwL/SRVNr4qjYEB5aBcfO6T8=";
     };
 
+    # pnpm downloads a node binary that matches the version specified in the
+    # package.json. I tried using that instead of system node, but I can't
+    # autoPatchElf it inside fetchPnpmDeps (that's a fixed-output derivation so
+    # can't depend on other store paths), but I also want it to have a real
+    # store path by the time I patchShebangs in this derivation to point at it.
+    # it turned out to be easier just to use system node, but let's at least
+    # check the major version is correct
     preConfigure = ''
       expected=v$(cut -d. -f1 .nvmrc)
       actual=$(${nodejs_pin}/bin/node --version | cut -d. -f1)
@@ -48,8 +55,10 @@ let
       fi
     '';
 
+    # see above: rm the downloaded binary because pnpm will put
+    # node_modules/.bin on our PATH when running scripts, and it doesn't work
+    # (having not been autoPatchElf'd)
     buildPhase = ''
-      # insist on our own node binary
       rm -f node_modules/.bin/node
 
       make build-web SHELL=${bash}/bin/bash
